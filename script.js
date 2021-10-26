0144978787caec97d2cf3675af7718d2b2d17713
vang007 <84373534+vang007@users.noreply.github.com>
2021-10-26 00:18:48 -0500

    completed

--- script.js
+++ script.js
@@ -1,57 +1,58 @@
 var generateBtn = document.querySelector("#generate");
-var lowerChar = "abcdefghijklmnopqrstuvwxyz";
-var capsChar = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
-var numberChar = "0123456789";
-var symbolsChar = "!#$%&'()*+-./:;<=>?@[\^_`{|}~";
+var smalls = "abcdefghijklmnopqrstuvwxyz";
+var biggie = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
+var numChar = "0123456789";
+var smblsChar = "!#$%&'()*+-./:;<=>?@[\^_`{|}~";
 
 function generatePassword() {
-  var password = "";
-  var passwordChar = "";
+    var password = "";
+    var passwordChar = "";
 
-  // prompt to select password length
-  var passwordLengthUser = prompt("How many would you like?");
-  passwordLengthUser = parseInt(passwordLengthUser);
-  if (passwordLengthUser < 8) {
+  // alert box to select number of characters for password //
+    var passwordLengthUser = prompt("How many would you like?");
+    passwordLengthUser = parseInt(passwordLengthUser);
+    if (passwordLengthUser < 8) {
     alert("Must have at least 8 characters!");
     return "";
-  }
-  if (passwordLengthUser > 128) {
+}
+    
+    if (passwordLengthUser > 128) {
     alert("Cannot exceed more than 128 characters!");
     return ""
-  }
-  //add capscase letters
-  var capsCharactersChoice = confirm("Include the capital letters?")
-  if (capsCharactersChoice) {
+}
+  //add upper case letters
+    var capsCharactersChoice = confirm("Include the capital letters?")
+    if (capsCharactersChoice) {
     passwordChar += capsChar;
-  }
-  //add lower letters
-  var lowerCharactersChoice = confirm("Include lowercase letters?");
-  if (lowerCharactersChoice) {
+}
+
+        //add lower letters
+    var lowerCharactersChoice = confirm("Include lowercase letters?");
+    if (lowerCharactersChoice) {
     passwordChar += lowerChar;
-
-  }
+}
   // add numbers
-  var numberCharactersChoice = confirm("Numbers?");
-  if (numberCharactersChoice) {
-    passwordChar += numberChar;
-  }
+    var numberCharactersChoice = confirm("Numbers?");
+    if (numberCharactersChoice) {
+    passwordChar += numChar;
+    }
   // add symbols characters
-  var symbolsCharacterChoice = confirm("Symbols/ Special Characters?");
-  if (symbolsCharacterChoice) {
+    var symbolsCharacterChoice = confirm("Symbols/ Special Characters?");
+    if (symbolsCharacterChoice) {
     passwordChar += symbolsChar;
-  }
-  for (var i = 0; i < passwordLengthUser; i++) {
+    }
+    for (var i = 0; i < passwordLengthUser; i++) {
     password += passwordChar[Math.floor(Math.random() * passwordChar.length)]
-  }
+    }
 
-  // Write password to the #password input
-  function writePassword(password) {
+  // show password
+    function writePassword(password) {
     var pwTextArea = document.getElementById("password");
 
     pwTextArea.value = password;
     return;
+}
 
-  }
-  writePassword(password)
+    writePassword(password)
 }
 generateBtn.addEventListener("click", generatePassword);
